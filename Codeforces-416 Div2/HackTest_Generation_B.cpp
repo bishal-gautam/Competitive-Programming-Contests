522e31275047d48513820db2f6f1930bde0b1934
BishalG <bsal.gautam16@gmail.com>
2017-05-27 17:44:15 +0600

    Cf 416 div2

---
+++ Codeforces-416 Div2/HackTest_Generation_B.cpp
@@ -0,0 +1,66 @@
+///==================================================///
+///                HELLO WORLD !!                    ///
+///                  IT'S ME                         ///
+///               BISHAL GAUTAM                      ///
+///         [ bsal.gautam16@gmail.com ]              ///
+///==================================================///
+#include<bits/stdc++.h>
+#define PI acos(-1.0)
+#define X first
+#define Y second
+#define mpp make_pair
+#define nl printf("\n")
+#define SZ(x) (int)(x.size())
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+///---------------------
+#define S(a) scanf("%d",&a)
+#define P(a) printf("%d",a)
+#define SL(a) scanf("%lld",&a)
+#define S2(a,b) scanf("%d%d",&a,&b)
+#define SL2(a,b) scanf("%lld%lld",&a,&b)
+///------------------------------------
+#define all(v) v.begin(),v.end()
+#define CLR(a) memset(a,0,sizeof(a))
+#define SET(a) memset(a,-1,sizeof(a))
+#define fr(i,a,n) for(int i=a;i<=n;i++)
+using namespace std;
+typedef long long ll;
+///int month[]={-1,31,28,31,30,31,30,31,31,30,31,30,31}; //Not Leap Year
+///int dx[]= {1,0,-1,0};int dy[]= {0,1,0,-1}; //4 Direction
+///int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 Direction
+///int dx[]= {2,1,-1,-2,-2,-1,1,2};int dy[]= {1,2,2,1,-1,-2,-2,-1}; //Knight Direction
+///==========CONSTANTS=============///
+///  Digit     0123456789012345678 ///
+#define MX     500005
+#define inf    1000000010
+#define MD     1000000007
+#define eps    1e-9
+///===============================///
+
+int n,x,m,i,ar[MX+2];
+int main(){
+    srand(129);
+    n=10000;
+    m=n;
+    cout<<n<< " "<<n<<endl;
+    for(i=1;i<=n;i++){
+        ar[i]=i;
+    }
+    random_shuffle(ar+1,ar+n+1);
+
+    cout<<ar[1];
+    for(i=2;i<=n;i++){
+        cout<< " "<<ar[i];
+    }
+    cout<<endl;
+
+    while(m--){
+        int rd=( rand()%n )+1;
+        cout<<1<< " "<<n<< " "<<rd<<endl;
+    }
+
+    return 0;
+}
+
